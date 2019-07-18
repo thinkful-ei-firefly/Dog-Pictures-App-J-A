a730ebe630e8de53cf005008c173540e6590b7d3
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-18 11:56:11 -0400

    add basic HTML and event listener for form submission

--- dog-app.js
+++ dog-app.js
@@ -1,1 +1,9 @@
 'use strict';
+//global $ //
+
+$('#dogForm').submit(function (event) {
+  console.log('listener fired!');
+  event.preventDefault();
+  let dogNum = $('#dogNum').val();
+  console.log(dogNum);
+});
