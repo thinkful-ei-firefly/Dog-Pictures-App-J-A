f5b061dcd8180db311fa3f125b17e97207117c0d
Ahmed <ahmed.max7586@gmail.com>
2019-07-18 12:10:01 -0400

    added fn

--- dog-app.js
+++ dog-app.js
@@ -7,3 +7,34 @@ $('#dogForm').submit(function (event) {
   let dogNum = $('#dogNum').val();
   console.log(dogNum);
 });
+
+
+function getDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(resp => resp.jason())
+    .then(respJson =>
+      displayResults(respJson))
+    .catch(error => alert('Something wrong. Try again'))
+}
+
+
+
+function displayResults(respJson) {
+
+  $('.results-img').replaceWith(
+    `<img src="${respJson.message}" class="results-img">`
+  )
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function () {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
